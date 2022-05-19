c36290e79ab5b39d021c840e1df0276b6cdfa631
DrixAbulad <Tietgalethehe123@gmail.com>
2022-05-19 16:46:11 +0800

    first commit

---
+++ Try/Abulad_FinalExam.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	char key[]={'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+	char answer [8][10] = {
+	{'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+	{'D', 'B', 'A', 'C', 'C', 'A', 'E', 'E', 'A', 'D'},
+	{'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+	{'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+	{'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+	{'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+	{'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+	{'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+};
+array_1 = a,b,c,d
+array_2 = b,b,c,a
+for (keme){
+if array_1[i] == array_2[i]
+score=+1;
+	printf("Student 0 Score: %d", score);
+	printf("\nStudent 1 Score: %d", score);
+	printf("\nStudent 2 Score: %d", score);
+	printf("\nStudent 3 Score: %d", score);
+	printf("\nStudent 4 Score: %d", score);
+	printf("\nStudent 5 Score: %d", score);
+	printf("\nStudent 6 Score: %d", score);
+	printf("\nStudent 7 Score: %d", score);
+
+	return score;
+}
+}
+
+
